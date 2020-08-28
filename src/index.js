365e72a3e8383783e9a5042d46901a754fe7e431
shahzebgit <shahzebparkar@gmail.com>
2020-08-28 14:39:45 +0530

    Added a cart ToggleHidden Feature with Apollo and GraphQL in local state
    Added a resolvers to get the extended queries

--- src/index.js
+++ src/index.js
@@ -6,15 +6,16 @@ import { PersistGate } from "redux-persist/integration/react";
 import { store, persistor } from "./redux/store";
 
 import { ApolloProvider } from "react-apollo";
-import { ApolloClient, gql } from "apollo-boost";
+import { ApolloClient } from "apollo-boost";
 import { createHttpLink } from "apollo-link-http";
 import { InMemoryCache } from "apollo-cache-inmemory";
+import { resolvers, typeDefs } from "./graphql//resolvers";
 
 import "./index.css";
 import App from "./App";
 
 const httpLink = createHttpLink({
-  uri: 'https://crwn-clothing.com',
+  uri: "https://crwn-clothing.com",
 });
 
 const cache = new InMemoryCache();
@@ -22,28 +23,18 @@ const cache = new InMemoryCache();
 const client = new ApolloClient({
   link: httpLink,
   cache,
+  resolvers,
+  typeDefs,
 });
 
-client
-  .query({
-    query: gql`
-    {
-      getCollectionsByTitle(title:"hats"){
-        id 
-        title
-        items{
-          id
-          name
-          price
-        }
-      }
-    }
-  `,
-  })
-  .then((res) => console.log(res));
+client.writeData({
+  data: {
+    cartHidden: true,
+  },
+});
 
 ReactDOM.render(
-  <ApolloProvider client={client} >
+  <ApolloProvider client={client}>
     <Provider store={store}>
       <BrowserRouter>
         <PersistGate persistor={persistor}>
